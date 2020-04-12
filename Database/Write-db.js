4b62bd19900e796504c276cf4674ead2d61b3bb8
Manas Chopra <manas.chopra@nagarro.com>
2020-04-13 01:28:37 +0530

    Populating DB

--- Database/Write-db.js
+++ Database/Write-db.js
@@ -7,14 +7,17 @@ async function task() {
 
     await Tasks.bulkCreate([
         { title:'NodeJS', description:'handles backend for applications',dueDate:'2020-04-15'},
-        { title:'AngularJS', description:'',dueDate:'2020-04-15',status:'incomplete',priority:'High'}
+        { title:'AngularJS', description:'',dueDate:'2020-04-15',status:'incomplete',priority:'High'},
+        { title:'Major Project', description:'',dueDate:'2020-05-01',priority:'High'}
 
       ])
     await Notes.bulkCreate([
-      {note:"do assignment", TaskId:"1"},
-      {note:"complete online course ", TaskId:"1"}, 
-      {note:"do assignment", TaskId:"2"},
-      {note:"complete online course", TaskId:"2"},
+      {note:"do assignment", TaskId:1},
+      {note:"complete online course ", TaskId:1}, 
+      {note:"do assignment", TaskId:2},
+      {note:"complete online course", TaskId:2},
+      {note:"make report", TaskId:3},
+      {note:"complete model", TaskId:3}
 
       ])
 }
