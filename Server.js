6c6d592dd9f3f750c3a44e13b7dc6236b5b3fc29
Manas Chopra <manas.chopra@nagarro.com>
2020-04-16 14:25:19 +0530

    updated route in Server.js

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/', express.static(__dirname + '/public'))
+app.use('/', express.static(__dirname + '/Public'))
 app.use('/tasks', taskRoute)
 app.use('/tasks', noteRoute)
 
